7b53ab77fda9a97b9fff4193f594f86f1a504f9f
Weiyu Du <weiyudu@client0807vag.vpn.private.upenn.edu>
2021-04-25 17:42:39 -0400

    commit

--- server/db-config.js
+++ server/db-config.js
@@ -1,8 +1,9 @@
 // Private. Will not be included in submission
 // Replace <Your Pennkey> and <Your Password>
 module.exports = {
-  host: "fling.seas.upenn.edu",
-  user: "",
-  password: "",
-  database: ""
+  host: "cis550-project.cac4ojhrtzw4.us-east-1.rds.amazonaws.com",
+  user: "projAdmin",
+  password: "5pr0j5_Bu$h0!",
+  port: '3306',
+  database: "ingr"
 };
--- server/routes.js
+++ server/routes.js
@@ -171,20 +171,29 @@ const getRecipes = (req, res) => {
 	console.log(recipeCount)
 	console.log(sortBy)
 
+	const query = `SELECT name, ratings
+	FROM recipe
+	ORDER BY ratings DESC
+	LIMIT 5;
+	`
+	console.log(query)
 	// TODO: write query and return 
 	// Follow the examples in hw2 to return 
-	// connection.query(query, (err, rows, fields) => {
-	//   if (err) console.log(err);
-	//   else res.json(results);
-	// });
+	connection.query(query, (err, rows, fields) => {
+		console.log(res)
+		if (err) console.log(err);
+		else res.json(results);
+	});
 
 	// TODO: DELETE THIS ONCE DONE IMPLEMENTING QUERIES 
 	// THIS IS PLACEHOLDER TO CHECK FETCH CALLS HERE
+	/*
 	const results = [
 		{ name: 'ramen', times: '10', ingredientCount: 2, stepCount: 5, rating: 5, ratingCount: 20, time: 10 },
 		{ name: 'fries', times: '20', ingredientCount: 1, stepCount: 4, rating: 2, ratingCount: 10, time: 20 }
 	]
 	res.json(results)
+	*/
 }
 
 const lowestTimePDV = (req, res) => {
@@ -251,20 +260,37 @@ const withIngredients = (req, res) => {
 	console.log(recipeCount)
 	console.log(sortBy)
 
+	const query = `WITH ingr_count AS
+	(
+		SELECT recipe_id, COUNT(*) AS num_ingredients
+		FROM has_ingr
+		GROUP BY recipe_id
+	)
+	SELECT r.name, r.ratings, r.minutes, ic.num_ingredients
+	FROM recipe r
+	JOIN ingr_count ic ON r.id = ic.recipe_id
+	WHERE name LIKE '%${ingredients}%' 
+	AND r.name NOT REGEXP '[0-9] [0-9]' 
+	AND r.minutes > 0
+	ORDER BY '${sortBy}'
+	LIMIT '${recipeCount}';`
+
 	// TODO: write query and return 
 	// Follow the examples in hw2 to return 
-	// connection.query(query, (err, rows, fields) => {
-	//   if (err) console.log(err);
-	//   else res.json(results);
-	// });
+	connection.query(query, (err, rows, fields) => {
+		if (err) console.log(err);
+		else res.json(results);
+	});
 
 	// TODO: DELETE THIS ONCE DONE IMPLEMENTING QUERIES 
 	// THIS IS PLACEHOLDER TO CHECK FETCH CALLS HERE
+	/*
 	const results = [
-		{ name: 'ramen', times: '10', ingredientCount: 2, stepCount: 5, rating: 5, ratingCount: 20, time: 10 },
+		{ name: 'hi', times: '10', ingredientCount: 2, stepCount: 5, rating: 5, ratingCount: 20, time: 10 },
 		{ name: 'fries', times: '20', ingredientCount: 1, stepCount: 4, rating: 2, ratingCount: 10, time: 20 }
 	]
 	res.json(results)
+	*/
 }
 
 const withNutritions = (req, res) => {
